b11abe96943bdc362ece17fb82260efd3717cfc6
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-02-25 21:05:28 +0100

    Update create_table_secondary_final.sql

--- create_table_secondary_final.sql
+++ create_table_secondary_final.sql
@@ -1,27 +1,27 @@
--- create table t_lucie_krivankova_project_SQL_secondary_final as (
-	with gdp as (
-		select 
+CREATE TABLE t_lucie_krivankova_project_SQL_secondary_final AS (
+	WITH gdp AS (
+		SELECT 
 			year,
 			country,
 			ROUND((gdp / 1000000000)::numeric, 2) AS gdp_in_billions
-		from economies
+		FROM economies
 	)
-	select 
+	SELECT
 		e.year,
 		e.country,
 		e.population,
 		e.gini,
 		g.gdp_in_billions,
-		lag(gdp_in_billions) over (partition by e.country order by e.year) as last_gdp
-	from countries c
-	left join economies e
-		on c.country = e.country
-	join gdp g
-		on g.year = e.year
-		and g.country = e.country
-	where 1=1
-		and c.continent = 'Europe'
-		and e.year between 2006 and 2018
-	order by e.country, e.year desc
---)
+		lag(gdp_in_billions) over (partition by e.country order by e.year) AS last_gdp
+	FROM countries c
+	LEFT JOIN economies e
+		ON c.country = e.country
+	JOIN gdp g
+		ON g.year = e.year
+		AND g.country = e.country
+	WHERE
+		c.continent = 'Europe'
+		AND e.year BETWEEN 2006 AND 2018
+	ORDER BY e.country, e.year desc
+)
 ;
