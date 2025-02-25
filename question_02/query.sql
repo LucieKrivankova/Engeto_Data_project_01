0e44c5c4c8d3fcfe73e22e18cc12951bd120bdc3
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-02-25 20:53:38 +0100

    Update query.sql

--- question_02/query.sql
+++ question_02/query.sql
@@ -8,8 +8,8 @@ select
 	round(avg(payroll_avg)/price_avg) as to_buy,
 	price_unit 
 from t_lucie_krivankova_project_SQL_primary_final
-where 1=1 
-	and (price_name like 'Mléko%' or price_name like 'Chl%')
+where 
+	(price_name like 'Mléko%' or price_name like 'Chl%')
 	and (year = 2006 or year = 2018)
 	group by year, price_name, price_avg, price_unit
 order by year, price_name
