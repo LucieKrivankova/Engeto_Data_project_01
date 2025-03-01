e4631a5f46782fcd6f8eb8526e8e912ad14534b7
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-03-01 17:31:47 +0100

    Update query.sql

--- question_03/query.sql
+++ question_03/query.sql
@@ -1,24 +1,24 @@
 /*Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?*/
 
-with price_data as (
-	select 
+WITH price_data AS (
+	SELECT
 		year,
 		price_name,
 		price_avg,
-		lag(price_avg) over (partition by price_name order by year),
+		LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year),
 		(
-		100 * (price_avg - lag(price_avg) over (partition by price_name order by year))
-		/lag(price_avg) over (partition by price_name order by year)
-		) as percent_change
-	from t_lucie_krivankova_project_SQL_primary_final
-	group by year, price_name, price_avg
+		100 * (price_avg - LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year))
+		/LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year)
+		) AS percent_change
+	FROM t_lucie_krivankova_project_SQL_primary_final
+	GROUP BY year, price_name, price_avg
 )
-select 
+SELECT
 	price_name,
-	round(avg(percent_change), 2) as avg_growth_rate
-from price_data
-group by price_name
-having round(avg(percent_change), 2) > 0
-order by avg_growth_rate 
-limit 1
+	round(avg(percent_change), 2) AS avg_growth_rate
+FROM price_data
+GROUP BY price_name
+HAVING round(avg(percent_change), 2) > 0
+ORDER BY avg_growth_rate 
+LIMIT 1
 ;
