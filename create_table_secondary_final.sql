edad18c05c07532c71d030a0761b0a389a1baaf7
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-03-01 17:25:49 +0100

    Update create_table_secondary_final.sql

--- create_table_secondary_final.sql
+++ create_table_secondary_final.sql
@@ -3,25 +3,25 @@ CREATE TABLE t_lucie_krivankova_project_SQL_secondary_final AS (
 		SELECT 
 			year,
 			country,
-			ROUND((gdp / 1000000000)::numeric, 2) AS gdp_in_billions
+			ROUND((gdp / 1000000000)::NUMERIC, 2) AS gdp_in_billions
 		FROM economies
 	)
-	SELECT
+	SELECT 
 		e.year,
 		e.country,
 		e.population,
 		e.gini,
 		g.gdp_in_billions,
-		lag(gdp_in_billions) over (partition by e.country order by e.year) AS last_gdp
+		LAG(gdp_in_billions) OVER (PARTITION BY e.country ORDER BY e.year) AS last_gdp
 	FROM countries c
 	LEFT JOIN economies e
 		ON c.country = e.country
 	JOIN gdp g
 		ON g.year = e.year
 		AND g.country = e.country
-	WHERE
-		c.continent = 'Europe'
+	WHERE 1=1
+		AND c.continent = 'Europe'
 		AND e.year BETWEEN 2006 AND 2018
-	ORDER BY e.country, e.year desc
+	ORDER BY e.country, e.year DESC
 )
 ;
