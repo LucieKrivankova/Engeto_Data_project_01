c7ff04c65d72ee342d91c6fe1878223259d5c2f9
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-03-01 17:39:11 +0100

    Update query.sql

--- question_05/query.sql
+++ question_05/query.sql
@@ -1,48 +1,45 @@
 /*Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce,
  *  projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?*/
 
-with gdp as (
-	select 
+WITH gdp AS (
+	SELECT
 		year,
-		round(
-		100 * (gdp_in_billions - last_gdp)
-		/ last_gdp,
-		2) as gdp_percent_change
-	from t_lucie_krivankova_project_SQL_secondary_final
-	where country like 'Cz%'
+		round(100 * (gdp_in_billions - last_gdp)/ last_gdp, 2) AS gdp_percent_change
+	FROM t_lucie_krivankova_project_SQL_secondary_final
+	WHERE country LIKE 'Cz%'
 ),
-price_data as (
-	select 
+price_data AS (
+	SELECT
 		year,
 		price_avg,
-		lag(price_avg) over (partition by price_name order by year) as last_price_avg,
+		LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year) AS last_price_avg,
 		(
-		100 * (price_avg - lag(price_avg) over (partition by price_name order by year))
-		/lag(price_avg) over (partition by price_name order by year)
-		) as price_percent_change
-	from t_lucie_krivankova_project_SQL_primary_final
-	group by year, price_name, price_avg
+		100 * (price_avg - LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year))
+		/LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year)
+		) AS price_percent_change
+	FROM t_lucie_krivankova_project_SQL_primary_final
+	GROUP BY year, price_name, price_avg
 ),
-payroll_data as (
-	select 
+payroll_data AS (
+	SELECT
 		year,
 		payroll_avg,
 		(
-		100 * (payroll_avg - lag(payroll_avg) over (partition by industry order by year))
-		/lag(payroll_avg) over (partition by industry order by year)
-		) as payroll_percent_change
-	from t_lucie_krivankova_project_SQL_primary_final
-	group by year, industry, payroll_avg
+		100 * (payroll_avg - LAG(payroll_avg) OVER (PARTITION BY industry ORDER BY year))
+		/LAG(payroll_avg) OVER (PARTITION BY industry ORDER BY year)
+		) AS payroll_percent_change
+	FROM t_lucie_krivankova_project_SQL_primary_final
+	GROUP BY year, industry, payroll_avg
 )
-select 
+SELECT
 	g.*,
-	round(avg(pr.price_percent_change), 2) as avg_price_per,
-	round(avg(pa.payroll_percent_change), 2) as avg_payroll_per
-from gdp g
-join price_data pr
-	on g.year = pr.year
-join payroll_data pa
-	on g.year = pa.year
-group by g.year, g.gdp_percent_change
-order by g.year desc
+	round(avg(pr.price_percent_change), 2) AS avg_price_per,
+	round(avg(pa.payroll_percent_change), 2) AS avg_payroll_per
+FROM gdp g
+JOIN price_data pr
+	ON g.year = pr.year
+JOIN payroll_data pa
+	ON g.year = pa.year
+GROUP BY g.year, g.gdp_percent_change
+ORDER BY g.year DESC
 ;
