603947fee31af5bfd077b6c1805c78fa639eb7e3
Lucie Křivánková <luciekrivankova.10@gmail.com>
2025-03-01 17:35:17 +0100

    Update query.sql

--- question_04/query.sql
+++ question_04/query.sql
@@ -1,38 +1,38 @@
 /*Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?*/
 
-with price_data as (
-	select 
+WITH price_data AS (
+	SELECT
 		year,
 		price_avg,
-		lag(price_avg) over (partition by price_name order by year) as last_price_avg,
+		LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year) AS last_price_avg,
 		(
-		100 * (price_avg - lag(price_avg) over (partition by price_name order by year))
-		/lag(price_avg) over (partition by price_name order by year)
-		) as price_percent_change
-	from t_lucie_krivankova_project_SQL_primary_final
-	group by year, price_name, price_avg
+		100 * (price_avg - LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year))
+		/LAG(price_avg) OVER (PARTITION BY price_name ORDER BY year)
+		) AS price_percent_change
+	FROM t_lucie_krivankova_project_SQL_primary_final
+	GROUP BY year, price_name, price_avg
 ),
-payroll_data as (
-	select 
+payroll_data AS (
+	SELECT
 		year,
 		payroll_avg,
 		(
-		100 * (payroll_avg - lag(payroll_avg) over (partition by industry order by year))
-		/lag(payroll_avg) over (partition by industry order by year)
-		) as payroll_percent_change
-	from t_lucie_krivankova_project_SQL_primary_final
-	group by year, industry, payroll_avg
+		100 * (payroll_avg - LAG(payroll_avg) OVER (PARTITION BY industry ORDER BY year))
+		/LAG(payroll_avg) OVER (PARTITION BY industry ORDER BY year)
+		) AS payroll_percent_change
+	FROM t_lucie_krivankova_project_SQL_primary_final
+	GROUP BY year, industry, payroll_avg
 )
-select 
+SELECT 
 	pr.year,
-	round(avg(pr.price_percent_change), 2) as avg_price_per,
-	round(avg(pa.payroll_percent_change), 2) as avg_payroll_per,
-	round(avg(pr.price_percent_change), 2) - round(avg(pa.payroll_percent_change), 2) as price_vs_payroll_change 
-from price_data pr
-join payroll_data pa
-	on pr.year = pa.year
-group by pr.year
-having round(avg(pr.price_percent_change), 2) - round(avg(pa.payroll_percent_change), 2) > 10
-order by pr.year
-limit 1
+	round(avg(pr.price_percent_change), 2) AS avg_price_per,
+	round(avg(pa.payroll_percent_change), 2) AS avg_payroll_per,
+	round(avg(pr.price_percent_change), 2) - round(avg(pa.payroll_percent_change), 2) AS price_vs_payroll_change 
+FROM price_data pr
+JOIN payroll_data pa
+	ON pr.year = pa.year
+GROUP BY pr.year
+HAVING round(avg(pr.price_percent_change), 2) - round(avg(pa.payroll_percent_change), 2) > 10
+ORDER BY pr.year
+LIMIT 1
 ;
